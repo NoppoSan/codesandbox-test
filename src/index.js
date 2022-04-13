1dd85d42311c3db7f710a0a73916d2586a9eab57
NoppoSan <noppo@positiveposition.org>
2022-04-14 00:40:09 +0900

    study spread

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+/**
+ * const, let等の変数宣言
+ */
+//const sayHello = (name = "ゲスト", num = 1) =>
+//  console.log(`${num}: こんにちは！${name}さん!`);
+//sayHello();
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/**
+ *  スプレッド構文 ...
+ */
+// 配列の展開
+/* const arr1 = [1, 2, 3];
+//console.log(arr1);
+//console.log(...arr1);
+
+const sumFunc = (num1, num2, num3) => console.log(num1 + num2 + num3);
+sumFunc(arr1[0], arr1[1], arr1[2]);
+sumFunc(...arr1); */
+
+// まとめる
+/* const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3); */
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+const arr8 = arr4;
+console.log(arr8);
+arr8[0] = 100;
+console.log(arr8); // 参照渡しによってarr4の値も変わる
